3c80e7254c22552486e0b299c5425c60cd8ba95a
midorinoyama <fujiaqa@gmail.com>
2021-08-10 06:28:10 +0000

    [Finish]3-7

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'bc50d7c305127625e156e1299f3292461e35426f55250c4e635b15058cf58ec02f68dec85da88b38eaa5e809e87922e19db7d1bddf7f3b29c2077ff605786e0b'
